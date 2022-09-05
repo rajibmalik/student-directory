0967fa6c6011c085d6568a96e566b647a59e53d1
rajibmalik <rajibmalik@hotmail.co.uk>
2022-09-05 20:02:35 +0000

    Refactored nested array into hash to make more readible

--- directory.rb
+++ directory.rb
@@ -1,16 +1,17 @@
-# First, we put our students into an array
+# First, we put our students into an array, this array is nested to include 
+# the cohort month as well as student name 
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 # Makes things easier to read by putting them into methods
 def print_header 
@@ -18,9 +19,9 @@ def print_header
     puts "-------------"
 end
 
-def print(names)
-    names.each do |name|
-        puts name
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end 
 
