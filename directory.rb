e01adb96207b112111c43cd0adf0ce40c361b1cb
rajibmalik <rajibmalik@hotmail.co.uk>
2022-09-05 20:17:30 +0000

    Refactored code to make interactive, user now inputs name of students

--- directory.rb
+++ directory.rb
@@ -13,6 +13,24 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end 
 # Makes things easier to read by putting them into methods
 def print_header 
     puts "The students of Villains Academy"
@@ -29,6 +47,8 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end 
 
+
+students = input_students
 # Nothing will happen until we call the methods we created
 print_header 
 print(students)
