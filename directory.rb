ccbb916fd21beb6ad2dc17c1517d5460a3e14dc8
rajibmalik <rajibmalik@hotmail.co.uk>
2022-09-06 20:42:10 +0000

    Added an option to save the list of students to a .csv file

--- directory.rb
+++ directory.rb
@@ -27,6 +27,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items 
 end 
 
@@ -42,6 +43,8 @@ def process(selection)
     input_students
   when "2"
     show_students 
+  when "3"
+    save_students 
   when "9"
     exit 
   else 
@@ -64,6 +67,26 @@ end
 def print_footer
     puts "Overall, we have #{@students.count} great students"
 end 
+
+def save_students
+  # Open the file for writing 
+  file = File.open("students.csv", "w")
+  # Iterate over the array of students 
+  @students.each do |student| 
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end 
+  file.close 
+end 
+
+
+
+
+
+
+
+
 
 interactive_menu
 
