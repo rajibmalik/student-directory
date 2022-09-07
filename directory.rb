4fecc1b59c1cc908c37b4a38caf1b474d312858c
rajibmalik <rajibmalik@hotmail.co.uk>
2022-09-07 16:56:44 +0000

    Added numbers before each name is printed using each.with_index

--- directory.rb
+++ directory.rb
@@ -59,8 +59,8 @@ def print_header
 end
 
 def print_students_list
-    @students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    @students.each.with_index(1) do |student, index|
+        puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
     end
 end 
 
